ba271789f05e27b512e8eb8243fdf2540f762d02
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2025-11-13 16:34:44 +0100

    make a card structure

---
+++ front-end/src/components/articleCard/ArticleCard.jsx
@@ -0,0 +1,27 @@
+
+
+
+function ArticleCard({cardImage, cardTitle, cardDescription, authorImage, authorName, datePost}){
+
+
+    return(
+        <div  className="article-card" >
+            <img src={cardImage} alt="empty" className="card-image" />
+            <div  className="card-content" >
+                <div  className="text-content" >
+                    <h2>{cardTitle}</h2>
+                    <p>{cardDescription}</p>
+                </div>
+                <div className="user-data" >
+                    <img src={authorImage} className="author-image" />
+                    <h2 className="author" >{authorName}</h2>
+                    <p className="date-post" >{datePost}</p>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+
+export default ArticleCard;
