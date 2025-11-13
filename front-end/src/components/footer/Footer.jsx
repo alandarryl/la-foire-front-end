8067d104f6e5690c1911549c5657067f0104ed2e
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2025-11-13 14:20:46 +0100

    complete navbar

--- front-end/src/components/footer/Footer.jsx
+++ front-end/src/components/footer/Footer.jsx
@@ -4,12 +4,12 @@ import "./footer.css";
 function Footer(){
     return(
         <footer className="footer" >
-            <p>© 2024 La foire. All rights reserved.</p>
             <div className="footer-links" >
                 <a href="#" >Privacy Policy</a>
                 <a href="#" >Terms of Service</a>
                 <a href="#" >Contact Us</a>
             </div>
+            <p>© 2024 La foire. All rights reserved.</p>
         </footer>
     )
 };
